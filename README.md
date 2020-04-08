2e2028f714731f73254be010c7a5e662514f26bd
Sian Culligan <SianCulligan@gmail.com>
2020-04-07 17:40:02 -0700

    code started not completed

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# 401-lab-07
+# Lab06
+
+Author: Sian Culligan
+
+Links and Resources
+GH PR:
+- https://github.com/SianCulligan/Lab06/pull/2
+
+Resource Links:
+- [Sonia's Lecture](https://www.youtube.com/watch?v=c7Fk4lbaMqM)
+- [Stack Overflow - to kick off json-server](https://stackoverflow.com/questions/33935281/command-not-found-oh-my-zsh)
+
+Swagger Documentation
+- [Swagger Doc Link](https://app.swaggerhub.com/apis-docs/SianCulligan/default-title/0.1)
+
+Setup/How to initialize & run your application 
+- run ``json-server --watch ./data/db.json`` from the root. 
+- enter ``http://localhost:3000/`` into your browser while the server is running
+
+How do you run tests?
+- [Go here](https://server-validation-lab-06.netlify.com/)
+- Enter ``http://localhost:3000/``
+
+Any tests of note?
+Describe any tests that you did not complete, skipped, etc
+- All tests pass
+
+![Tests Passing](./assets/passing.png)
---
+++ __tests__/server.test.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const app = require('../lib/server.js');
+const supergoose = require('@code-fellows/supergoose');
+
+const mockRequest = supergoose(app.server);
+
+describe('product routes work', () => {
+  it('can GET (read) products', async () => {
+    let response = await mockRequest.get('/products');
+    //check screen shot at 209 for other way of calling the about line
+    console.log('GET body',response.body);
+    console.log('GET status', response.status);
+    //look for status, statusCode, &/or statusMessage in 'response'
+    expect(JSON.stringify(response.body)).toBe(JSON.stringify([
+      { id: 1, category: 'shirt', name: 'Black shirt', display_name: 'Black short sleeve shirt', description: 'Stay Home Club Black Shirt'},
+      { id: 2, category: 'pants', name: 'Black pants', display_name: 'Distressed black pants', description: 'Topshop black pants'},
+      { id: 3, category: 'flat_shoes', name: 'Black flats', display_name: 'Black print shoes', description: 'Louis Et Cie black flats'},
+      {category: 'sweatshirt', name: 'Black sweatshirt', display_name: 'Black long sleeve fleece', description: 'Express black lace up fleece', id: 4},
+    ]),
+    ),
+    expect(response.status).toBe(200);
+  });
+
+  it('can POST (create) products', async () => {
+    let originalLength = data.products.length;
+
+    console.log('original', originalLength);
+    let newProductData = JSON.stringify({
+      'category': 'tank top',
+      'name': 'White tank top',
+      'display_name': 'White sleeveless shirt',
+      'description': 'H&M white tank top',
+    });
+    let response = await mockRequest.post('/products');
+
+    console.log('TEST TWO RESPONSE', response.body.id);
+    expect(response.body.id).toBe()
+  });
+});
+
+
+
+//   it('can update products', async () => {
+
+//     
+
+//     expext(JSON.stringify(id: 1, name: 'Black shirt',display_name': 'Black short sleeve shirt' ))
+//     //finish the test at 2:34 - this is corrected (test at 229 will not pass)
+// });
+// }
+
+
+// describe('middelware works', () => {
+//     it('gives a 404 error when accessing', async () => {
+//         let response = .status
+//     })
+//     //see 251 screenshot for 404 test
+// })
